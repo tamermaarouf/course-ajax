336f923fe5428e5c5d3785dd159ad990fb4b60ba
tamer_maarouf <tamermaarouf@gmail.com>
2021-11-11 05:13:40 +0200

    add Image from unsplash

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -9,4 +9,16 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
     });
+const searchedForText = 'hippos';
+const unsplashRequest = new XMLHttpRequest();
+unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+unsplashRequest.onload = addImage;
+unsplashRequest.setRequestHeader('Authorization', 'WYl48orpG-39aMBtEcQxFUU-IVg_ljidN8ul4fFxzhM');
+unsplashRequest.send();
+
+function addImage(){
+    
+}
 })();
+
+
